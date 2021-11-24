ccf333a0fbf48d1548ca22d17baf35432fa39210
Brais Muñiz <mc.brais@gmail.com>
2021-11-24 09:19:38 +0100

    Adds default files within the module defaults_lp

--- crystal_tree/crystal_tree.py
+++ crystal_tree/crystal_tree.py
@@ -3,18 +3,10 @@ from sklearn.tree._tree import Tree
 
 import numpy as np
 
-_DEFAULT_TRACES= \
-"""%!trace {"Good forecast (>5 years)", P} alive(P).
-%!trace {"Bad forecast (<5years)", P} not_alive(P).
+from . import defaults_lp
 
-%!trace {"% is true",F} holds(P,F,true).
-%!trace {"% is false",F} holds(P,F,false). 
-
-%!trace {"% > %", F, T} gt(P,F,T).
-%!trace {"% <= %", F, T} le(P,F,T).
-%!trace {"% in (%,%]", F, Min, Max} between(P,F,Min,Max)."""
-
-_DEFAULT_EXTRA_LOCATION="default_extra.lp"
+_DEFAULT_EXTRA_LOCATION  = "default_extra.lp"
+_DEFAULT_TRACES_LOCATION = "default_traces.lp"
 
 class CrystalTree():
        
@@ -32,32 +24,44 @@ class CrystalTree():
         self.feature = self._tree.feature
         self.threshold = self._tree.threshold
         self.value = self._tree.value
-        
+
+        if feature_names is None:
+            self.feature_names = [f'f{i}' for i in range(max(self.feature))]
+        else:
+            if len(feature_names) != max(self.feature)+1:
+                raise ValueError("Feature names length does not match tree data.")
+            self.feature_names = feature_names
+
         self.feature_names = feature_names
-        
+
     @property
     def traces(self):
         if not hasattr(self, '_traces'):
-            with open(_DEFAULT_TRACES_LOCATION, 'r') as traces_files:
-                setattr(self, '_traces', traces_files.read())
+            try:
+                import importlib.resources as pkg_resources
+            except ImportError:
+                # Try backported to PY<37 `importlib_resources`.
+                import importlib_resources as pkg_resources
+            setattr(self, '_traces', pkg_resources.read_text(defaults_lp, _DEFAULT_TRACES_LOCATION))
         return self._traces
 
     @property
     def extra(self):
         if not hasattr(self, '_extra'):
-            with open(_DEFAULT_TRACES_LOCATION, 'r') as extra_files:
-                setattr(self, '_extra', extra_files.read())
+            try:
+                import importlib.resources as pkg_resources
+            except ImportError:
+                # Try backported to PY<37 `importlib_resources`.
+                import importlib_resources as pkg_resources
+            setattr(self, '_extra', pkg_resources.read_text(defaults_lp, _DEFAULT_EXTRA_LOCATION))
         return self._extra
-
         
     def _thresholds(self):
-        return "\n".join([f'thres({int(v*100)}).' for v in set(self.threshold) if v>0]) 
+        return "\n".join([f'thres({int(v*100)}).' for v in set(self.threshold) if v>0])
 
     def which_class(self, val):
-        if val[0][0] >= val[0][1]:
-            return "alive(P)"
-        else:
-            return "not_alive(P)"
+        max_index = list(val[0]).index(max(val[0]))
+        return f'class({max_index},P)'
 
     def __write_rule(self, stack):
         nid, _ = stack.pop()
@@ -65,20 +69,16 @@ class CrystalTree():
 
         literals = []
         min_max = {
-            "rec_afp": {"min_gt": -np.inf, "max_le": np.inf},
-            "don_microesteatosis": {"min_gt": -np.inf, "max_le": np.inf},
-            "rec_provenance": {"min_gt": -np.inf, "max_le": np.inf},
+            f: {"min_gt": -np.inf, "max_le": np.inf} for f in self.feature_names
         }
         while stack:
             nid, right = stack.pop()
             f, t = self.feature_names[self.feature[nid]], self.threshold[nid]
-            if f in min_max:
-                if right and t>min_max[f]["min_gt"]:
-                    min_max[f]["min_gt"]=t
-                if not right and t<min_max[f]["max_le"]:
-                    min_max[f]["max_le"]=t
-            else:
-                literals.append(f'holds(P,{f},{"true" if right else "false"})')
+
+            if right and t>min_max[f]["min_gt"]:
+                min_max[f]["min_gt"]=t
+            if not right and t<min_max[f]["max_le"]:
+                min_max[f]["max_le"]=t
 
         for f, vs in min_max.items():
             if not np.isinf(vs["min_gt"]) and not np.isinf(vs["max_le"]):
@@ -90,7 +90,7 @@ class CrystalTree():
 
         rule = f'{head} :- {", ".join(literals)}.'
 #         print(rule)
-        return rule       
+        return rule
 
     def _rule_per_leaf(self, stack=None):
         if stack == None:
@@ -109,61 +109,6 @@ class CrystalTree():
             program=self._rule_per_leaf(),
             thresholds=self._thresholds(),
         )
-
-    def rule_per_node_escalonado(self, include_traces=True):
-        node = lambda node, side: f'tree_node({node},P,{side})'
-        write_holds = lambda f, v: f'holds(P,{f},{v})'
-        gt = lambda f, v: f'gt(P,{f},{int(v*100)})'
-        le = lambda f, v: f'le(P,{f},{int(v*100)})'
-
-        write_bool_trace = lambda f, v: f'{f} is {v}'
-        write_gt_trace = lambda f, v: f'{f} > {v}'
-        write_le_trace = lambda f, v: f'{f} <= {v}'
-        trace = lambda s: f'%!trace_rule {{"{s}"}}'
-
-        def visit_nodes(stack):
-            if not stack:
-                nid = 0            
-            else:
-                parent, side = stack[-1]
-                nid = self.children_left[parent] if side == "left" else self.children_right[parent]
-
-            f = self.feature_names[self.feature[nid]] if self.feature_names else 2
-            t = self.threshold[nid]
-            if self.children_left[nid] == self.children_right[nid]:
-                return f'{self.which_class(self.value[nid])} :- {node(parent, side)}.\n'
-            else:
-                parent_condition = "" if nid == 0 else ", "+node(parent, side)
-                if f in ("rec_afp", "don_microesteatosis", "rec_provenance"):
-                    left_trace_rule = trace(write_le_trace(f, t))
-                    node_left_condition  = le(f, t)
-                    right_trace_rule = trace(write_gt_trace(f, t))
-                    node_right_condition = gt(f, t) 
-                else:
-                    left_trace_rule = trace(write_bool_trace(f, "false"))
-                    node_left_condition  = write_holds(f, "false")
-                    right_trace_rule = trace(write_bool_trace(f, "true"))
-                    node_right_condition = write_holds(f, "true")
-
-                # %!trace_rule {"{nombre variable} is true/false"}
-                # %!trace_rule {"{nombre variable} <=/> num"}
-
-                return "{trace_rule}\n{head} :- {condition}{parent}.\n\n".format(
-                    trace_rule=left_trace_rule if include_traces else "",
-                    head=node(nid, "left"),
-                    condition=node_left_condition,
-                    parent=parent_condition
-                ) + \
-                visit_nodes(stack + [(nid, "left")]) + \
-                "{trace_rule}\n{head} :- {condition}{parent}.\n\n".format(
-                    trace_rule=right_trace_rule if include_traces else "",
-                    head=node(nid, "right"),
-                    condition=node_right_condition,
-                    parent=parent_condition
-                ) + \
-                visit_nodes(stack + [(nid, "right")])
-
-        return visit_nodes([])
     
     def write_in_files(self, paths="paths.lp", extra="extra.lp", traces="traces.lp"):
         if extra is not None:
